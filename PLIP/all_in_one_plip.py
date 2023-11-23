aff0aff15fa88cec5af309fe96f0e510d8abeeeb
JeongSoo Na <skwjdtn3171@naver.com>
2023-11-23 10:07:49 +0900

    feat js

--- PLIP/all_in_one_plip.py
+++ PLIP/all_in_one_plip.py
@@ -43,11 +43,59 @@ for i in input_list:
 ### Report to csv table
 f = open(output_dir + "/plip_value_table.csv")
 f.write("PDB,Interaction_type,RESNR,RESTYPE,DIST,DIST_sub\n")
-
-
-
-
+for i in range(0,len(input_list)):
+# for i in range(0,1):
+    r = open(output_dir + "/" + input_list[i].replace(".pdb","") + "/report.txt")
+    report = r.read()
+    
+    # Hydrophobic Interactions
+    if "**Hydrophobic Interactions**" in report:
+        len = report.split("**Hydrophobic Interactions**\n")
+        for j in range(1,len):
+            tmp = report.split("**Hydrophobic Interactions**\n")[j].split("\n")[0].split("+")
+            report_hi = report.split("+" + tmp[1].replace("-","=") + "+" + tmp[2].replace("-","=") + "+" + tmp[3].replace("-","=") + "+" + tmp[4].replace("-","=") + "+" + tmp[5].replace("-","=") + "+" + tmp[6].replace("-","=") + "+" + tmp[7].replace("-","=") + "+" + tmp[8].replace("-","=") + "+" + tmp[9].replace("-","=") + "+" + tmp[10].replace("-","=") + "+" + tmp[11].replace("-","=") + "+\n")[1].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+" + "\n\n")[0].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+")
+            # print(len(report_hi))
+            for k in range(0,len(report_hi)):
+                RESNR = report_hi[k].split("|")[1].replace(" ","")
+                RESTYPE = report_hi[k].split("|")[2].replace(" ","")
+                DIST = report_hi[k].split("|")[7].replace(" ","")
+                ### updating
+                f.write(input_list[i].replace(".pdb","") + ",Hydrophobic Interactions," + RESNR + "," + RESTYPE + "," + DIST + ",," + "\n")
+            
+    
+    # Hydrogen Bonds
+    if "**Hydrogen Bonds**" in report:
+        len = report.split("**Hydrogen Bonds**\n")
+        for j in range(1,len):
+            tmp = report.split("**Hydrogen Bonds**\n")[j].split("\n")[0].split("+")
+            report_hb = report.split("+" + tmp[1].replace("-","=") + "+" + tmp[2].replace("-","=") + "+" + tmp[3].replace("-","=") + "+" + tmp[4].replace("-","=") + "+" + tmp[5].replace("-","=") + "+" + tmp[6].replace("-","=") + "+" + tmp[7].replace("-","=") + "+" + tmp[8].replace("-","=") + "+" + tmp[9].replace("-","=") + "+" + tmp[10].replace("-","=") + "+" + tmp[11].replace("-","=") + "+" + tmp[12].replace("-","=") + "+" + tmp[13].replace("-","=") + "+" + tmp[14].replace("-","=") + "+" + tmp[15].replace("-","=") + "+" + tmp[16].replace("-","=") + "+" + tmp[17].replace("-","=") + "+\n")[1].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+" + tmp[12] + "+" + tmp[13] + "+" + tmp[14] + "+" + tmp[15] + "+" + tmp[16] + "+" + tmp[17] + "+" + "\n\n")[0].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+" + tmp[12] + "+" + tmp[13] + "+" + tmp[14] + "+" + tmp[15] + "+" + tmp[16] + "+" + tmp[17] + "+")
+            # print(len(report_hb))
+            for k in range(0,len(report_hb)):
+                RESNR = report_hb[k].split("|")[1].replace(" ","")
+                RESTYPE = report_hb[k].split("|")[2].replace(" ","")
+                DIST = report_hb[k].split("|")[8].replace(" ","")
+                DIST_sub = report_hb[k].split("|")[9].replace(" ","")
+                f.write(input_list[i].replace(".pdb","") + ",Hydrogen Bonds," + RESNR + "," + RESTYPE + "," + DIST + "," + DIST_sub + "\n")
+            
+    
+    
+    # Salt Bridges
+    if "**Salt Bridges**" in report:
+        len = report.split("**Salt Bridges**\n")
+        for j in range(1,len):
+            tmp = report.split("**Salt Bridges**\n")[j].split("\n")[0].split("+")
+            report_sb = report.split("+" + tmp[1].replace("-","=") + "+" + tmp[2].replace("-","=") + "+" + tmp[3].replace("-","=") + "+" + tmp[4].replace("-","=") + "+" + tmp[5].replace("-","=") + "+" + tmp[6].replace("-","=") + "+" + tmp[7].replace("-","=") + "+" + tmp[8].replace("-","=") + "+" + tmp[9].replace("-","=") + "+" + tmp[10].replace("-","=") + "+" + tmp[11].replace("-","=") + "+" + tmp[12].replace("-","=") + "+" + tmp[13].replace("-","=") + "+\n")[1].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+" + tmp[12] + "+" + tmp[13] + "+" + "\n\n")[0].split("\n" + "+" + tmp[1] + "+" + tmp[2] + "+" + tmp[3] + "+" + tmp[4] + "+" + tmp[5] + "+" + tmp[6] + "+" + tmp[7] + "+" + tmp[8] + "+" + tmp[9] + "+" + tmp[10] + "+" + tmp[11] + "+" + tmp[12] + "+" + tmp[13] + "+")
+            # print(len(report_sb))
+            for k in range(0,len(report_sb)):
+                RESNR = report_sb[k].split("|")[1].replace(" ","")
+                RESTYPE = report_sb[k].split("|")[2].replace(" ","")
+                DIST = report_sb[k].split("|")[8].replace(" ","")
+                f.write(input_list[i].replace(".pdb","") + ",Salt Bridges," + RESNR + "," + RESTYPE + "," + DIST + ",," + "\n")
 
 f.close()
+
+### I will update the Calculation Process & Filtering System
+### Update code clearly
+### MIT License
 
 os.chdir(current_path)
